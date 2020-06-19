89722994391219f7b6e3c23f6d023aba19e68a1f
cjgl <114471855@qq.com>
2020-06-19 15:34:57 +0800

    modify

--- src/main/java/cn/fy/cjgl/controller/PermissionController.java
+++ src/main/java/cn/fy/cjgl/controller/PermissionController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.PageHelper;
 
